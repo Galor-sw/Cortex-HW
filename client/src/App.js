929a76f1df4af454c9422c163861a1923340f9a2
galor <galor@metgo.app>
2025-07-07 16:13:07 +0300

    workflow ci/cd check

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - changes
+          Learn React - changes 2
         </a>
       </header>
     </div>
