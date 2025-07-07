05a3fb6baa1536b4bcf41419217dbd0f95eb5db9
galor <galor@metgo.app>
2025-07-07 16:30:52 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - changes
+          Learn React - changes to check
         </a>
       </header>
     </div>
