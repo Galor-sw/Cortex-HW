73e49bc4e30724dc334a81d8ae00cfac21c68f01
galor <galor@metgo.app>
2025-07-09 17:28:23 +0300

    cloud functions GET function added

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,21 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cors = require("cors");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const corsHandler = cors({ origin: true });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.getTrafficData = functions.https.onRequest((req, res) => {
+    corsHandler(req, res, async () => {
+        try {
+            const db = admin.firestore();
+            const snapshot = await db.collection("trafficStats").get();
+            const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            res.status(200).json(data);
+        } catch (error) {
+            console.error("Error fetching traffic stats:", error);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+});
